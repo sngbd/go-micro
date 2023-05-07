e440ba2bfaafe831cf6414c04c87ea530f4b0229
Gilberdi Sinaga <gilberdising012@gmail.com>
2023-05-07 18:00:29 +0800

    feat: implement rpc

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -53,7 +53,7 @@ func connect() (*amqp.Connection, error) {
 	
 	// don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp:guest//guest@rabbitmq")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
 			fmt.Println("RabbitMQ not yet ready...")
 			counts++
